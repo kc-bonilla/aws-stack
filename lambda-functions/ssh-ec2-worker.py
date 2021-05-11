dca583e8e23215a8dbcf2af796b8c9c56b8aa239
KC Bonilla <74749648+kc-bonilla@users.noreply.github.com>
2021-05-10 22:51:33 -0500

    Create ssh-ec2-worker.py

---
+++ lambda-functions/ssh-ec2-worker.py
@@ -0,0 +1,53 @@
+import os
+import boto3
+import paramiko
+from time import sleep
+
+
+HOST = "3.16.70.231"
+USER = "kcbonilla"
+INSTANCE_ID = "i-0efe038e2ebc73df1"
+S3_BUCKET = "datapipeline-analytics-shootproof-com"
+BUCKET_KEY = "step-functions/scripts/pendo_ec2_key.pem"
+PKEY_FILE = "/tmp/pendo_ec2_key.pem"
+COMMANDS = [
+    "echo 'ssh to ec2 instance successful'",
+    "ls -a"
+]
+
+
+def execute_cmds(ssh=None, cmds=None):
+    for cmd in cmds:
+        stdin, stdout, stderr = ssh.exec_command(cmd)
+        stdout_reader(stdout)
+        print(stdout.read().decode('utf-8').strip("\n"))
+
+
+def ssh_connect_ec2():
+    privkey = paramiko.RSAKey.from_private_key_file(PKEY_FILE)
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=HOST, username=USER, pkey=privkey)
+    return ssh
+
+
+def fetch_pkey():
+    """Download private key file from secure S3 bucket"""
+    s3_client = boto3.client('s3')
+    s3_client.download_file(S3_BUCKET, BUCKET_KEY, PKEY_FILE)
+    pkey_filename = PKEY_FILE.replace("/tmp/", "")
+    if os.path.isfile(PKEY_FILE):
+        return print(f"{pkey_filename} successfully downloaded from {S3_BUCKET}")
+
+
+def main():
+    privkey = fetch_pkey()
+    ssh = ssh_connect_ec2()
+    execute_cmds(ssh, COMMANDS)
+    ssh.close()
+    instance.stop()
+    return print("Script execution completed. See Cloudwatch logs for complete output")
+
+
+def worker_handler(event, context):
+    main()
